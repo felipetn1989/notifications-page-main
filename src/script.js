4cf5b97c1abd9bf281b9921a9187687a04e3436f
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-02-23 16:12:13 -0300

    script finished

--- src/script.js
+++ src/script.js
@@ -1,5 +1,27 @@
-const unreads = document.querySelectorAll(".unread")
+let unreads = document.querySelectorAll(".unread");
+let unreadDots = document.querySelectorAll(".unread_dot");
 
-unreads.forEach(unread => {
-    unread.addEventListener("click",() => alert('test'))
-})
+notificationsNumber.innerHTML = unreads.length;
+
+unreads.forEach((unread, index) => {
+  unread.addEventListener("click",() => displayAsRead(unread,index));
+});
+
+markAll.addEventListener("click", () => {
+  unreads.forEach((unread, index) => displayAsRead(unread,index));
+});
+
+function displayAsRead(unread,index) {
+  unreadDots[index].classList.remove(
+    "unread_dot",
+    "after:w-2",
+    "after:h-2",
+    "after:inline-block",
+    "after:bg-[#f65351]",
+    "after:rounded-full"
+  );
+  unread.classList.replace("bg-[#f7fafd]", "bg-white");
+  unread.classList.remove("unread");
+  unreads = document.querySelectorAll(".unread");
+  notificationsNumber.innerHTML = unreads.length;
+}
